79d8091851b94210b34464d1dced23f09cb5db9a
Ankit <ankitmaggon@gmail.com>
2025-07-20 11:58:09 +0530

    Adding dependency

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -4,27 +4,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Demo1Application extends SpringBootServletInitializer{
-	
-	private static final Logger logger =LoggerFactory.getLogger(Demo1Application.class);
-	
-	@Override
-	protected SpringApplicationBuillder configure(SpringApplicationBuillder application)
-	{
-		application.sources(Demo1Application.class);
-	}
-	public static void main(String[] args) {
-		logger.info("this is a info message");
-		logger.error("this is a error message");
-		logger.warn("this is a warning message");
-		SpringApplication.run(Demo1Application.class, args);
-		
-	
-	}
-	
+public class Demo1Application {
+
+    private static final Logger logger=LoggerFactory.getLogger(Demo1Application.class);
+
+    public static void main(String[] args) {
+        logger.info("this is a info message");
+        logger.error("this is a error message");
+        logger.warn("this is a warning message");
+        SpringApplication.run(Demo1Application.class, args);
+
+
+    }
+
 
 }
