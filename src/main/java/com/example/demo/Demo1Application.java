b0e48459fb8542729c5067d2afc9eb44037475f8
ankit-maggon <51758813+ankit-maggon@users.noreply.github.com>
2025-07-20 17:33:37 +0530

    Revert "Feature/lambda expression"

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -4,14 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
+@RestController
 public class Demo1Application {
 
     private static final Logger logger=LoggerFactory.getLogger(Demo1Application.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         logger.info("this is a info message");
         logger.error("this is a error message");
         logger.warn("this is a warning message");
